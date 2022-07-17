79e6043029e81886ef27a94a600324e6c706351c
Haydenull <brooklyn1984@qq.com>
2022-07-17 17:53:57 +0800

    add route

--- src/f2estatic/f2estatic.controller.ts
+++ src/f2estatic/f2estatic.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { F2estaticService } from './f2estatic.service';
 
 @Controller('f2estatic')
-export class F2estaticController {}
+export class F2estaticController {
+  constructor(private readonly f2estaticService: F2estaticService) {}
+
+  @Get('project')
+  async getProject() {
+    const data = await this.f2estaticService.getData();
+    return data;
+  }
+
+  @Get('project/:id')
+  async getProjectById(@Param('id') id: string) {
+    const data = await this.f2estaticService.getData();
+    return data.find(item => item.id === id);
+  }
+
+  @Post('project')
+  async createProject(@Body() body) {
+    return body;
+  }
+
+  @Put('project/:id')
+  async updateProject(@Param('id') id: string, @Body() body) {
+    return body;
+  }
+
+}
